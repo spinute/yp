5f3134a0e959003e186be618c2b53a928bda13e2
TakefumiYamamura <yama1223xxx@gmail.com>
2018-05-13 12:46:08 +0000

    fix run_bench.sh

--- script/run_bench.sh
+++ script/run_bench.sh
@@ -3,12 +3,12 @@
 solver=$1
 bench=$2
 
-log=log/$solver
+log=log/${bench}_$solver
 
 if [ -f $log ]; then
 	rm $log
 fi
 
 for fname in ./benchmarks/$bench/*; do
-	(time ./$solver $fname) >> ./log/${bench}_${solver} 2>&1
+	(time ./$solver $fname) >> $log 2>&1
 done
